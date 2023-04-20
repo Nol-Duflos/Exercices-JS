7aaef8ac96618daa7528e42355a6ad1f79e91655
Nolwenn <duflos.nol@laposte.net>
2023-04-20 14:15:18 +0200

    Le DOM

---
+++ Le Dom/app.js
@@ -0,0 +1,5 @@
+//Le DOM c'est toute notre structure HTML
+//En JavaScript, on peut manipuler le DOM, lui ajouter des éléments, lui en retirer, ajouter des noeuds, etc.
+//Notamment en les selectionnant
+
+
