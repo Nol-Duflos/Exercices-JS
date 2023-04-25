7cc9f821be546348adfc91f59c586d64d17f5706
Nolwenn <duflos.nol@laposte.net>
2023-04-25 15:36:31 +0200

    Maniulation d'Api

--- API/app.js
+++ API/app.js
@@ -8,18 +8,35 @@
 const liste =  document.querySelector('.liste');
 const btn = document.querySelector('.btn');
 
-//Appel basique avec XHR
-//Chaque fois, on fait des appels avec des URL :
-
-
 const xhr = new XMLHttpRequest();
 
-xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+btn.addEventListener('click', () => {
+    console.log("Hello world");
+    
+    
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
 
-//Fontion pour m'anipuler l'appel d'API
-xhr.onload = function(){
-    console.log(JSON.parse(xhr.response)); //Permet de log l'appel sur console.log sous forme de chaine de caractère
-                                            //JSON.parse transforme le Json en JavaScript
-}
+    xhr.responseType = 'json'; //Indique que c'est json qui est appelé, 
+    
+    //Fontion pour m'anipuler l'appel d'API
+    xhr.onload = function(){
+        console.log(xhr.response); //Permet de log l'appel sur console.log sous forme de chaine de caractère
+        //JSON.parse transforme le Json en JavaScript
 
-xhr.send(); //Pour voir l'appel effectué, il faut aller dans "reseaux" "Network" 
+        for(i = 0; i < xhr.response.length; i++) {
+
+            let newLi = document.createElement('li');
+            let newTitreCarte = document.createElement('h2');
+            let newBodyCarte = document.createElement('p');
+
+            newTitreCarte.innerText = xhr.response[i].title;
+            newBodyCarte.innerText = xhr.response[i].body;
+
+            newLi.appendChild(newTitreCarte);
+            newLi.appendChild(newBodyCarte);
+            liste.appendChild(newLi);
+        }
+    }
+    
+    xhr.send(); //Pour voir l'appel effectué, il faut aller dans "reseaux" "Network" 
+})
