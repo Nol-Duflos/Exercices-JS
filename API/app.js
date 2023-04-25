101b1c32affe7f84b2d7076e48ac785227ea6c06
Nolwenn <duflos.nol@laposte.net>
2023-04-25 14:10:29 +0200

    Premier commit de la journée

--- API/app.js
+++ API/app.js
@@ -5,11 +5,21 @@
 //A surpassé XML
 
 
+const liste =  document.querySelector('.liste');
+const btn = document.querySelector('.btn');
+
 //Appel basique avec XHR
 //Chaque fois, on fait des appels avec des URL :
+
 
 const xhr = new XMLHttpRequest();
 
 xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
 
-xhr.send();
+//Fontion pour m'anipuler l'appel d'API
+xhr.onload = function(){
+    console.log(JSON.parse(xhr.response)); //Permet de log l'appel sur console.log sous forme de chaine de caractère
+                                            //JSON.parse transforme le Json en JavaScript
+}
+
+xhr.send(); //Pour voir l'appel effectué, il faut aller dans "reseaux" "Network" 
